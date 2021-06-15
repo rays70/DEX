386e15396ca46edb352099d167c6903a88788605
rays70 <subhasis1.roy@gmail.com>
2021-06-15 11:22:29 +0100

    Create token.sol

---
+++ contracts/token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Link is ERC20 {
+
+    constructor() ERC20("ChainLink", "LINK") public {
+
+        _mint(msg.sender, 5000); 
+    }
+
+}
